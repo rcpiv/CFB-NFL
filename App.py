3f4e2b5dbcdcf7caeafa6af4ad9edd8d5a2a7471
Robert C Patterson <67168947+rcpiv@users.noreply.github.com>
2021-01-12 13:17:15 -0500

    Added error handling for user input

--- App.py
+++ App.py
@@ -30,37 +30,47 @@ def filter_list(league):
         NFL (National Football League)
         CFB (College Football (FBS))
     """
-    # User inout
-    ui = input("{l} Team: ".format(l = league.name)).title()
-    
-    # Tells python that we want this variable to exist outside of the function too
-    global selection
-    
-    # Checks to see if selection exists already
-    try:
-        selection
-    except NameError:
-        selection = {}
-    
-    # Filters team names that start with string given    
-    filtered = [x for x in league.Winner.unique().tolist() if x.startswith(ui)]
-    
-    # Gives list of team names that start with the string given
-    if len(filtered) > 1:
-        print('There are more than one team starting with "{0}"'.format(ui))
-        print('Select from choices: ')
-        for index, name in enumerate(filtered):
-            print("{0}: {1}".format(index,name))
-            
-        index = int(input('Enter choice number: '))
-        selection[league.name] = filtered[index]
-        print('Selected team: {0}'.format(selection[league.name]))
-        print()
-    else:
-        print(filtered[0])
-        selection[league.name] = filtered[0]
-        print('Selected team: {0}'.format(selection[league.name]))
-        print()
+    while True:
+        # User input
+        ui = input("{l} Team: ".format(l = league.name)).title()
+        
+        # Tells python that we want this variable to exist outside of the function too
+        global selection
+        
+        # Checks to see if selection exists already
+        try:
+            selection
+        except NameError:
+            selection = {}
+        
+        # Filters team names that start with string given    
+        filtered = [x for x in league.Winner.unique().tolist() if x.startswith(ui)]
+        
+        # Gives list of team names that start with the string given
+        if len(filtered) > 1:
+            print('There are more than one team starting with "{0}"'.format(ui))
+            print('Select from choices: ')
+            for index, name in enumerate(filtered):
+                print("{0}: {1}".format(index,name))
+                
+            while True:
+                try:
+                    index = int(input('Enter choice number: '))
+                    selection[league.name] = filtered[index]
+                    break
+                except IndexError:
+                    print('Invalid Selection. Please select from choices given.')
+            print('Selected team: {0}'.format(selection[league.name]))
+            print()
+            break
+        elif len(filtered) == 1:
+            print(filtered[0])
+            selection[league.name] = filtered[0]
+            print('Selected team: {0}'.format(selection[league.name]))
+            print()
+            break
+        else:
+            print('Invalid Entry. Please try another.')
 #%% Test
 filter_list(cfb)
 filter_list(nfl)
@@ -83,15 +93,18 @@ print('The last time {NFL} and {CFB} won the same week was {Date}.'\
 
 #%% Do it with a function
 
-def get_dates(n):
+def get_dates():
     """
-    Gets the most frequent date(s) of desired combination
+    Gets the most recent date(s) of desired combination
 
     Returns
     -------
     None.
 
     """
+    filter_list(cfb)
+    filter_list(nfl)
+    
     dfs = {
     1 : cfb[cfb['Winner'] == selection['CFB']],
     2 : nfl[nfl['Winner'] == selection['NFL']],
@@ -99,24 +112,54 @@ def get_dates(n):
     4 : nfl[nfl['Loser'] == selection['NFL']]}
     
     # User chooses what they want to see
-    choices = ['Both NFL and CFB win','NFL win and CFB lose','NFL lose and CFB win','Both NFL and CFB lose']
+    # NFL: 0 CFB: 1
+    choices = ['Both {0} and {1} win','{0} win and {1} lose','{0} lose and {1} win','Both {0} and {1} lose']
+    teams = [selection['NFL'],selection['CFB']]
+    choices2 = []
+    for i in choices:
+        choices2.append(i.format(*teams))
     print('What combination do you wish to select?') 
-    for index, name in enumerate(choices):
+    for index, name in enumerate(choices2):
             print("{0}: {1}".format(index,name))
-    ui = input('Select from choices above: ')   
+    global ui
+    while True:
+        ui = int(input('Select from choices above: '))   
     
-    global df
-    
-    if ui == 0:
-        df = pd.merge(dfs[1],dfs[2], on='WeekID', how='inner')
-    elif ui == 1:
-        df = pd.merge(dfs[2],dfs[3], on='WeekID', how='inner')
-    elif ui == 2:
-        df = pd.merge(dfs[1],dfs[4], on='WeekID', how='inner')
-    else:
-        df = pd.merge(dfs[3],dfs[4], on='WeekID', how='inner')
+        global df
+        global cfb_out
+        global nfl_out
         
-    dict_of_dates = {'NFL Date':[],'CFB Date':[]}
+        if ui == 0:
+            df = pd.merge(dfs[1],dfs[2], on='WeekID', how='inner')
+            cfb_out = 'won'
+            nfl_out = 'won'
+            break
+        elif ui == 1:
+            df = pd.merge(dfs[2],dfs[3], on='WeekID', how='inner')
+            cfb_out = 'lost'
+            nfl_out = 'won'
+            break
+        elif ui == 2:
+            df = pd.merge(dfs[1],dfs[4], on='WeekID', how='inner')
+            cfb_out = 'won'
+            nfl_out = 'lost'
+            break
+        elif ui == 3:
+            df = pd.merge(dfs[3],dfs[4], on='WeekID', how='inner')
+            cfb_out = 'lost'
+            nfl_out = 'lost'
+            break
+        else:
+            print('Invalid Selection. Select a number between 0 and 3')
+        
+    
+    print()
+    print('The last time {NFL} {NFL_Out} and {CFB} {CFB_Out} the same week was {Date}.'\
+      .format(NFL=selection['NFL'], \
+              CFB=selection['CFB'], \
+              Date = max(max(pd.to_datetime(df['Date'])),\
+                         max(pd.to_datetime(df['start_date']))).strftime('%B %d, %Y'),\
+          CFB_Out = cfb_out, NFL_Out = nfl_out))
     
     
 
